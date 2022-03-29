e003733faa90220332fe2168b82d27826939070b
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2022-03-29 19:03:54 +0200

    Create todo list component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  readonly taskToDoTitle: string = 'Tasks to do';
+  readonly taskDoneTitle: string = 'Tasks done';
+
+  readonly toDoTasks: Array<string> = ['1', '2', '3'];
+  readonly doneTasks: Array<string> = ['4', '5'];
 }
