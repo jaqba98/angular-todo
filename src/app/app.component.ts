c7d5094ba7dd23e124470ffe5873d6dcb321fb04
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2022-05-04 21:59:19 +0200

    Refactor the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { filter, map, Observable } from 'rxjs';
-import { TaskModel } from './models/task.model';
-import { TasksStoreService } from './services/tasks-store.service';
 
 @Component({
   selector: 'app-root',
@@ -9,15 +6,4 @@ import { TasksStoreService } from './services/tasks-store.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  displayTest$: Observable<string>;
-  toDoTasks: Observable<Array<TaskModel>>;
-  doneTasks: Array<TaskModel>;
-  
-  constructor(private readonly tasksStoreSrv: TasksStoreService) {}
-  
-  taskToDoTitle: string = 'Tasks to do';
-  taskDoneTitle: string = 'Tasks done';
-
-  // toDoTasks: Array<TaskModel> = this.tasksStoreSrv.getTasks().filter(task => !task.done);
-  // doneTasks: Array<TaskModel> = this.tasksStoreSrv.getTasks().filter(task => task.done);
 }
